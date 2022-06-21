8bd7b5e073b4799cbed6a8fdcf8fdb2593c1fbfb
Yui <hejianping2015@outlook.com>
2022-06-21 22:23:59 +0800

    unit test

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+import unittest
+
+from BeautifulReport import BeautifulReport
+
 from Test.CustomerTest import CustomerTest
 from Test.MarketingSupervisorTest import MarketingSupervisorTest
 from Test.MarketingTest import MarketingTest
@@ -6,79 +10,180 @@ from Test.SignerTest import SignerTest
 from Test.TestingSupervisorTest import TestingSupervisorTest
 from Test.TestingTest import TestingTest
 
+
+class MyTestCase(unittest.TestCase):
+    def testcase1(self):
+        """用户新建并填写委托"""
+        c = CustomerTest()
+        c.new_delegation()
+        c.fill_delegation()
+        c.go_to_function_list()
+        c.fill_function_list()
+        c.quit()
+        pass
+
+    def testcase2(self):
+        """市场部主管分配人员"""
+        ms = MarketingSupervisorTest()
+        ms.distribute_delegation()
+        ms.quit()
+        pass
+
+    def testcase3(self):
+        """测试部主管分配人员"""
+        ts = TestingSupervisorTest()
+        ts.distribute_delegation()
+        ts.quit()
+        pass
+
+    def testcase4(self):
+        """市场部审核委托"""
+        m = MarketingTest()
+        m.audit_delegation()
+        m.quit()
+        pass
+
+    def testcase5(self):
+        """市场部审核委托"""
+        t = TestingTest()
+        t.audit_delegation()
+        t.quit()
+        pass
+
+    def testcase6(self):
+        """市场部生成报价"""
+        m = MarketingTest()
+        m.generate_pricing()
+        m.quit()
+        pass
+
+    def testcase7(self):
+        """客户接受报价"""
+        c = CustomerTest()
+        c.accept_pricing()
+        c.quit()
+        pass
+
+    def testcase8(self):
+        """市场部填写合同"""
+        m = MarketingTest()
+        m.fill_contract()
+        m.quit()
+        pass
+
+    def testcase9(self):
+        """客户检查合同"""
+        c = CustomerTest()
+        c.check_contract()
+        c.quit()
+        pass
+
+    def testcase10(self):
+        """客户填写合同"""
+        c = CustomerTest()
+        c.fill_contract()
+        c.quit()
+        pass
+
+    def testcase11(self):
+        """市场部审核合同"""
+        m = MarketingTest()
+        m.audit_contract()
+        m.quit()
+        pass
+
+    def testcase12(self):
+        """市场部上传合同"""
+        m = MarketingTest()
+        m.upload_contract()
+        m.quit()
+        pass
+
+    def testcase13(self):
+        """测试部主管填写项目编号"""
+        ts = TestingSupervisorTest()
+        ts.fill_project_id()
+        ts.quit()
+        pass
+
+    def testcase14(self):
+        """客户上传样品"""
+        c = CustomerTest()
+        c.upload_sample()
+        c.quit()
+        pass
+
+    def testcase15(self):
+        """市场部验收样品"""
+        m = MarketingTest()
+        m.audit_sample()
+        m.quit()
+        pass
+
+    def testcase16(self):
+        """测试部填写测试方案"""
+        t = TestingTest()
+        t.fill_solution()
+        t.quit()
+        pass
+
+    def testcase17(self):
+        """质量部审核测试方案"""
+        q = QualityTest()
+        q.audit_solution()
+        q.quit()
+        pass
+
+    def testcase18(self):
+        """测试部填写测试文档"""
+        t = TestingTest()
+        t.fill_document()
+        t.quit()
+        pass
+
+    def testcase19(self):
+        """测试部主管审核测试报告"""
+        ts = TestingSupervisorTest()
+        ts.audit_report()
+        ts.quit()
+        pass
+
+    def testcase20(self):
+        """客户审核测试报告"""
+        c = CustomerTest()
+        c.audit_report()
+        c.quit()
+        pass
+
+    def testcase21(self):
+        """授权签字人审核报告"""
+        s = SignerTest()
+        s.sign()
+        s.quit()
+        pass
+
+    def testcase22(self):
+        """测试部归档测试文档"""
+        t = TestingTest()
+        t.archive_report()
+        t.quit()
+        pass
+
+    def testcase23(self):
+        """市场部发送测试报告"""
+        m = MarketingTest()
+        m.send_report()
+        m.quit()
+        pass
+
+    def testcase24(self):
+        """客户接收测试报告"""
+        c = CustomerTest()
+        c.receive_report()
+        c.quit()
+        pass
+
+
 if __name__ == '__main__':
-    c = CustomerTest()
-    c.new_delegation()
-    c.fill_delegation()
-    c.go_to_function_list()
-    c.fill_function_list()
-    c.quit()
-    ms = MarketingSupervisorTest()
-    ms.distribute_delegation()
-    ms.quit()
-    ts = TestingSupervisorTest()
-    ts.distribute_delegation()
-    ts.quit()
-    m = MarketingTest()
-    m.audit_delegation()
-    m.quit()
-    t = TestingTest()
-    t.audit_delegation()
-    t.quit()
-    m = MarketingTest()
-    m.generate_pricing()
-    m.quit()
-    c = CustomerTest()
-    c.accept_pricing()
-    c.quit()
-    m = MarketingTest()
-    m.fill_contract()
-    m.quit()
-    c = CustomerTest()
-    c.check_contract()
-    c.quit()
-    c = CustomerTest()
-    c.fill_contract()
-    c.quit()
-    m = MarketingTest()
-    m.audit_contract()
-    m.quit()
-    m = MarketingTest()
-    m.upload_contract()
-    m.quit()
-    ts = TestingSupervisorTest()
-    ts.fill_project_id()
-    ts.quit()
-    c = CustomerTest()
-    c.upload_sample()
-    c.quit()
-    m = MarketingTest()
-    m.audit_sample()
-    m.quit()
-    t = TestingTest()
-    t.fill_solution()
-    t.quit()
-    q = QualityTest()
-    q.audit_solution()
-    q.quit()
-    t = TestingTest()
-    t.fill_document()
-    t.quit()
-    ts = TestingSupervisorTest()
-    ts.audit_report()
-    ts.quit()
-    c = CustomerTest()
-    c.audit_report()
-    c.quit()
-    s = SignerTest()
-    s.sign()
-    s.quit()
-    t = TestingTest()
-    t.archive_report()
-    t.quit()
-    m = MarketingTest()
-    m.send_report()
-    m.quit()
-    c = CustomerTest()
-    c.receive_report()
-    c.quit()
+    br = BeautifulReport(unittest.defaultTestLoader.loadTestsFromTestCase(MyTestCase))
+    br.report(filename='report', description='report', report_dir='.')
